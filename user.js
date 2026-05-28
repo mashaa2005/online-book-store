46dc0dd347258e7dcd02050a6feddf5440868f24
mashaa2005 <masapenev2005@gmail.com>
2026-05-28 13:19:47 +0200

    Delete user.js

--- user.js
+++
@@ -1,6 +0,0 @@
-
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://masapenev2005_db_user:<db_password>@cluster0.x07ngos.mongodb.net/?appName=Cluster0")
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error(err));
