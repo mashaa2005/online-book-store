e69f92ce17484c17978b4a0724edf671bee5f069
mashaa2005 <masapenev2005@gmail.com>
2026-05-28 13:22:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,66 +4,33 @@ document.addEventListener("DOMContentLoaded", function () {
     const orderForm = document.getElementById("orderForm");
 
     if (orderForm) {
-
-        orderForm.addEventListener("submit", async function (e) {
-
+        orderForm.addEventListener("submit", function (e) {
             e.preventDefault();
 
             const name = document.getElementById("name").value;
-            const address = document.getElementById("address").value;
 
-            try {
+            alert(`${name}, благодарим за поръчката!`);
 
-                const response = await fetch("/orders", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({
-                        customerName: name,
-                        address: address,
-                        productId: "book"
-                    })
-                });
-
-                const data = await response.json();
-
-                alert(data.message);
-
-                orderForm.reset();
-
-            } catch (error) {
-
-                console.log(error);
-
-            }
-
+            orderForm.reset();
         });
-
     }
-
 
     const reviewForm = document.getElementById("reviewForm");
 
     if (reviewForm) {
-
         reviewForm.addEventListener("submit", function (e) {
-
             e.preventDefault();
 
             const reviewText = document.getElementById("reviewText");
             const reviewsBox = document.getElementById("reviews");
 
-            let newReview = document.createElement("p");
-
+            const newReview = document.createElement("p");
             newReview.textContent = reviewText.value;
 
             reviewsBox.prepend(newReview);
 
             reviewText.value = "";
-
         });
-
     }
 
 });
